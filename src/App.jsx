5c9492e5845356d9ba9cfcb32904d45e460805e8
waleed khan <waleedkhansafimd983@gmail.com>
2025-08-22 02:10:57 -0700

    creating routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-export default function App() {
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import About from './pages/About'
+import Profile from './pages/Profile'
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline text-red-600">
-      Hello world!
-    </h1>
+     <BrowserRouter>
+     <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/about' element={<About />} />
+      <Route  path='/profile' element={<Profile />} />
+     </Routes>
+     </BrowserRouter>
   )
 }
+
+export default App
